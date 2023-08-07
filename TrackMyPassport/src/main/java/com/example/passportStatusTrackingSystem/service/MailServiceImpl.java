fb6942c994266ba46f58b83ff1aecb9d822d334c
CS445-545 <140013855+CS445-545@users.noreply.github.com>
2023-08-07 12:28:17 -0400

    Update MailServiceImpl.java

--- TrackMyPassport/src/main/java/com/example/passportStatusTrackingSystem/service/MailServiceImpl.java
+++ TrackMyPassport/src/main/java/com/example/passportStatusTrackingSystem/service/MailServiceImpl.java
@@ -1,24 +1,20 @@
-/**
-This service class provides implementations for sending emails using the Spring Boot JavaMailSender.
-*/
-
 package com.example.passportStatusTrackingSystem.service;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
 import com.example.passportStatusTrackingSystem.model.Mail;
 
+/**
+This service class provides implementations for sending emails using the Spring Boot JavaMailSender.
+*/
 @Service("mailService")
 public class MailServiceImpl implements MailService {
 
@@ -36,27 +32,22 @@ public class MailServiceImpl implements MailService {
 	public int sendEmail(Mail mail, long applicantId) {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		int flag = 0;
-
 		try {
 			FileSystemResource file = new FileSystemResource(
 					new File("C:\\Deveopment_avecto\\" + applicantId + "simple.pdf"));
 			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-
 			mimeMessageHelper.setSubject(mail.getMailSubject());
 			mimeMessageHelper.setFrom(new InternetAddress(mail.getMailFrom(), "passportSystem.com"));
 			mimeMessageHelper.setTo(mail.getMailTo());
 			mimeMessageHelper.setText(mail.getMailContent());
 			mimeMessageHelper.addAttachment("E-Passport.pdf", file);
-
 			mailSender.send(mimeMessageHelper.getMimeMessage());
 			flag = 1;
-
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-
 		return flag;
 	}
 
@@ -70,25 +61,19 @@ public class MailServiceImpl implements MailService {
 	public int sendEmail(Mail mail) {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		int flag = 0;
-
 		try {
-
 			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
-
 			mimeMessageHelper.setSubject(mail.getMailSubject());
 			mimeMessageHelper.setFrom(new InternetAddress(mail.getMailFrom(), "passportSystem.com"));
 			mimeMessageHelper.setTo(mail.getMailTo());
 			mimeMessageHelper.setText(mail.getMailContent());
-
 			mailSender.send(mimeMessageHelper.getMimeMessage());
 			flag = 1;
-
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-
 		return flag;
 	}
 }
