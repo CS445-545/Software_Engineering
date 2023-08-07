47ade0d65c6b9e2206962f70f1f9874dcbd3e1e4
CS445-545 <140013855+CS445-545@users.noreply.github.com>
2023-08-07 12:10:49 -0400

    Update Login_detailsRepository.java

--- TrackMyPassport/src/main/java/com/example/passportStatusTrackingSystem/repository/Login_detailsRepository.java
+++ TrackMyPassport/src/main/java/com/example/passportStatusTrackingSystem/repository/Login_detailsRepository.java
@@ -1,6 +1,3 @@
-// This repository interface handles data access and database interactions for the Login_details entity.
-// It extends JpaRepository, providing out-of-the-box CRUD operations for the entity.
-
 package com.example.passportStatusTrackingSystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,14 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import com.example.passportStatusTrackingSystem.model.Login_details;
 
+/*This repository interface handles data access and database interactions for the Login_details entity.
+It extends JpaRepository, providing out-of-the-box CRUD operations for the entity.*/
 public interface Login_detailsRepository extends JpaRepository<Login_details, String> {
 
-	// Custom query to find login details by email ID.
+	/* Custom query to find login details by email ID. */
 	@Query("select ld from Login_details ld where email_id=:email_id")
 	public Login_details findByEmailId(@Param("email_id") String email_id);
 
-	// Custom query to find login details by email ID and password.
+	/*Custom query to find login details by email ID and password.*/
 	@Query("select ad from Login_details ad where ad.email_id=:emailId and password=:password")
 	public Login_details findByEmailIdandPassword(@Param("emailId") String emailId, @Param("password") String password);
-
 }
